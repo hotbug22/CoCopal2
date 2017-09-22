377a673eb8b868ce0c5e65ce27d0be2290d2c4cb
hotbug22 <hotbug22@gmail.com>
2017-09-22 13:41:40 +0900

    훌쩍훌쩍

---
+++ app/src/main/java/com/example/a60029506/cocopal/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.a60029506.cocopal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        /*joosuk joosuk*/
+    }
+}
